bcef00bec002004eecb3eded78fbc5eff7637e01
ohad <ohadtay@gmail.com>
2022-03-23 16:34:42 +0200

    adding data loader part 1

---
+++ SpyFishAotearoaDataset.py
@@ -0,0 +1,14 @@
+import torch
+from PIL import Image
+
+
+class SpyFishAotearoaDataset(torch.utils.data.Dataset):
+    def __init__(self, images_path, masks_path):
+        self.images = list()
+        pass
+
+    def __getitem__(self, idx):
+        pass
+
+    def __len__(self):
+        pass
