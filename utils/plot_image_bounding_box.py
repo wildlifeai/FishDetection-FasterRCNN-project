92991506be019619056a0a85c842795afe9bfa3e
Adi Gabay <adigaby@cs.huji.ac.il>
2022-04-20 15:09:42 +0300

    Update plot_image_bounding_box.py

--- utils/plot_image_bounding_box.py
+++ utils/plot_image_bounding_box.py
@@ -19,10 +19,11 @@ def get_transform(train):  #TODO: remove when done with simulation
     return T.Compose(transforms)
 
 
-def add_bounding_boxes(img, pred_cls, pred_score, boxes, rect_th=2, text_size=1, text_th=1,
-                       color_box=(255, 0, 0)):
+def add_bounding_boxes(img, pred_cls, pred_score, boxes, thresh=0.5, rect_th=2, text_size=0.8, text_th=1,
+                       color_box=(255, 255, 255)):
     """
     Returns the image with boxes and labels as PIL
+    :param thresh:
     :param img: The image
     :param pred_cls: Array of classes (as numbers)
     :param pred_score: array of probabilities
@@ -42,16 +43,19 @@ def add_bounding_boxes(img, pred_cls, pred_score, boxes, rect_th=2, text_size=1,
     pred_score = pred_score.numpy()
 
     for i in range(len(boxes)):
+        if pred_score[i] < thresh:
+            continue
         cv2.rectangle(img, (int(boxes[i][0]), int(boxes[i][1])),
-                      (int(boxes[i][0] + boxes[i][3]),
-                       int(boxes[i][1] + boxes[i][2])),
+                      (int(boxes[i][2]),
+                       int(boxes[i][3])),
                       color=color_box, thickness=rect_th)
-        cv2.putText(img, "fish" + ": " + str(round(pred_score[i], 3)) + "%",
+        cv2.putText(img, "fish" + ":" + str(round(pred_score[i], 3)),
                     (int(boxes[i][0]), int(boxes[i][1])),
-                    cv2.FONT_HERSHEY_SIMPLEX, text_size, color_box, thickness=text_th)  # TODO: add real class label
+                    cv2.FONT_HERSHEY_DUPLEX, text_size, color_box, thickness=text_th)  # TODO: add real class label
 
     # turn to PIL
     return Image.fromarray((img * 255).astype(np.uint8))
+
 
 if __name__ == '__main__':
     # TODO: remove when done with simulation
@@ -63,23 +67,7 @@ if __name__ == '__main__':
         images = list(image for image in images)
         targets = [{k: v for k, v in t.items()} for t in targets]
 
-        img = add_bounding_boxes(images[0], torch.Tensor([4]), torch.Tensor([80]),  targets[0]["boxes"])
-
-        # img = images[0].numpy().T.swapaxes(0,1)
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        # boxes = targets[0]["boxes"]
-
-        # for i in range(len(boxes)):
-        #     cv2.rectangle(img, (int(boxes[i][0]), int(boxes[i][1])),
-        #                   (int(boxes[i][0] + boxes[i][3]),
-        #                   int(boxes[i][1] + boxes[i][2])),
-        #                   color=(0, 255, 0), thickness=2)
-        #     cv2.putText(img, "fish" + ": " + str(50), (int(boxes[i][0]), int(boxes[i][1])),
-        #                 cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), thickness=1) # TODO: add real label
-        #     # pred_cls[i]
-
+        img = add_bounding_boxes(images[0], torch.Tensor([4]), torch.Tensor([0.8]),  targets[0]["boxes"])
 
         plt.imshow(img)
         plt.xticks([])
@@ -87,15 +75,3 @@ if __name__ == '__main__':
         plt.show()
         break
 
-    #boxes, pred_cls, pred_score = get_prediction(img_path, confidence)
-    # img = cv2.imread("../first_image/Images/c9a75cb0-80b1-4298-b16c-992f43852ff3.jpeg")
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # # print(len(boxes))
-    # # for i in range(len(boxes)):
-    # #     cv2.rectangle(img, boxes[i][0], boxes[i][1], color=(0, 255, 0), thickness=rect_th)
-    # #     cv2.putText(img, pred_cls[i] + ": " + str(round(pred_score[i], 3)), boxes[i][0],
-    # #                 cv2.FONT_HERSHEY_SIMPLEX, text_size, (0, 255, 0), thickness=text_th)
-    # plt.imshow(img)
-    # plt.xticks([])
-    # plt.yticks([])
-    # plt.show()
